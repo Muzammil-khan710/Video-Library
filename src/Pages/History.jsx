a0cc84ea9172ef42a792ab87f3c2dfa96eeb72b6
Muzammil khan <khanmuzammil549@gmail.com>
2022-09-05 00:20:40 +0530

    Feat : History page

--- src/Pages/History.jsx
+++ src/Pages/History.jsx
@@ -1,18 +1,67 @@
-import React from 'react'
-import { Header, Sidebar } from '../Components'
+import React from "react";
+import { Header, Sidebar } from "../Components";
+import { useLike } from "../Context/LikeContext";
+import { Link } from "react-router-dom";
+import { useVideo } from "../Context/VideoContext";
+import { LikeFill, LikeIcon, PlaylistIcon, WatchLater, WatchLaterFill, DeleteIcon } from "../Assets/AllSvg";
+import { useWatchLater } from "../Context/WatchlaterContext";
+import { useHistory } from "../Context/HistoryContext";
 
 const HistoryPage = () => {
+  const { playerHandler } = useVideo();
+
+  const { likeVid, likeToggler } = useLike();
+
+  const { watchLaterToggler, watchLaterVideos  } = useWatchLater()
+
+  const { historyVideo, removeFromHistory, removeAllHistory } = useHistory()
+
   return (
     <div>
-        <Header/>
-        <div className='flex'>
-        <Sidebar/>
-        <div className='container  flex flex-col flex-wrap gap-4 p-2 mt-[6rem] ml-28 sm:ml-40 md:ml-44'>
-            <div className='text-white self-center '>History page</div>
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <div className="container  flex  flex-wrap gap-4 p-2 mt-[6rem] ml-28 sm:ml-40 md:ml-44">
+          <div className="container flex mt-3 mx-auto ">
+          <button className="p-[1rem] mx-auto bg-[#334756]  hover:bg-slate-500 active:bg-slate-400' rounded-md text-white" onClick={removeAllHistory}>Clear History</button>
+          </div>
+          {historyVideo.map(({ title, date, views, creator, image, _id }) => {
+            return (
+              <div
+                className="border p-5 m-3 bg-slate-400 rounded-md w-72"
+                key={_id}
+              >
+                <Link to={`/player/${_id}`} onClick={() => playerHandler(_id)}>
+                  <img className="h-40 w-full rounded-md" src={image} alt="" />
+                  <div className="text-2xl">{title}</div>
+                  <div className="font-medium">{creator}</div>
+                  <div className="flex gap-2 justify-between ">
+                    {views} views <span>{date}</span>{" "}
+                  </div>
+                </Link>
+                <div className="flex justify-around mt-2">
+                  <button onClick={() => likeToggler(_id)}>
+                    {likeVid.find((item) => item._id === _id) ? (
+                      <LikeFill />
+                    ) : (
+                      <LikeIcon />
+                    )}
+                  </button>
+                  <button>
+                    <PlaylistIcon />
+                  </button>
+                  <button onClick={() => watchLaterToggler(_id)}>
+                  {watchLaterVideos.find((item) => item._id === _id) ? ( <WatchLaterFill /> ) : ( <WatchLater />) }
+                </button>
+                <button onClick={() => removeFromHistory(_id)}><DeleteIcon/></button>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export { HistoryPage }
+export { HistoryPage };
