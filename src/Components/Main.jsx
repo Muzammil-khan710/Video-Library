093b4a2823b263228d7d66e2cd9b1a38425a327a
Muzammil khan <khanmuzammil549@gmail.com>
2022-09-27 00:20:35 +0530

    Fixed : remove unused code

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -20,51 +20,21 @@ const Main = () => {
 
   const { watchLaterVideos, watchLaterToggler } = useWatchLater();
 
-  const { addToHistory} = useHistory()
+  const { addToHistory } = useHistory()
  
   const userFromLocal = localStorage.getItem("user")
-
-  return (
-    <div className="container  flex flex-wrap gap-4 p-2 mt-[6rem] ml-28 sm:ml-40 md:ml-44">
-      {videoList.map(
-        ({ _id, title, image, views, date, creator }) => {
-          return (
-            <div
-              className="border p-5 m-3 bg-slate-400 rounded-md w-72"
-              key={_id}
-            >
-              <Link to={`/player/${_id}`} onClick={() => { playerHandler(_id) ; addToHistory(_id) }}>
-                <img className="h-40 w-full rounded-md" src={image} alt="" />
-                <div className="text-2xl">{title}</div>
-                <div className="font-medium">{creator}</div>
-                <div className="flex gap-2 justify-between ">
-                  {views} views <span>{date}</span>{" "}
-                </div>
-              </Link>
-              <div className="flex justify-around mt-2">
-                <button onClick={() => likeToggler(_id)}>
-                  { userFromLocal ? ( likeVid.find((item) => item._id === _id) ? ( <LikeFill /> ) : ( <LikeIcon />) ) : ( <LikeIcon/>)}
-                </button>
-                <button>
-                  <PlaylistIcon />
-                </button>
-                <button onClick={() => watchLaterToggler(_id)}>
-                  { userFromLocal ? ( watchLaterVideos.find((item) => item._id === _id) ? ( <WatchLaterFill /> ) : ( <WatchLater />) ) : ( <WatchLater/>)}
-                </button>
-
-  const userFromLocal = localStorage.getItem("user");
 
   const {  setOpenModal, setCurrentModalId } = usePLaylist()
 
   return (
-    <div className="container  flex flex-wrap  p-2 mt-[6rem] ml-28 sm:ml-40 md:ml-44">
+    <div className="container  flex flex-wrap gap-4 p-2 mt-[6rem] ml-28 sm:ml-40 md:ml-44">
       {videoList.map(({ _id, title, image, views, date, creator }) => {
         return (
           <div
             className="border p-5 m-3 bg-slate-400 rounded-md w-72 static"
             key={_id}
           >
-            <Link to={`/player/${_id}`} onClick={() => playerHandler(_id)}>
+            <Link to={`/player/${_id}`} onClick={() => { playerHandler(_id); addToHistory(_id) }}>
               <img className="h-40 w-full rounded-md" src={image} alt="" />
               <div className="text-2xl">{title}</div>
               <div className="font-medium">{creator}</div>
